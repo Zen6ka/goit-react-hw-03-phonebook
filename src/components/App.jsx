f01cd884644cc48c4f22ea0bd9fe8cd125d2bc80
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2024-01-23 16:24:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ContactForm } from "./ContactForm/ContactForm";
 import {ContactList} from "./ContactList/ContactList"
-import {Filter} from "./Filters/Filter"
+import {Filter} from "./Filter/Filter"
 import { Container, Title } from './App.styled'
 import { GlobalStyle } from "GlobalStyle";
 
