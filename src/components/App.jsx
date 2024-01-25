e5851f1ff8f0255f4390cd233a2d4ff4f1080fb4
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2024-01-25 14:11:37 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,20 @@ export class App extends Component {
 filter: ''
 }
 
+componentDidMount() {
+	const contacts = localStorage.getItem('contacts');
+	const parseContacts = JSON.parse(contacts);
+	if (parseContacts) {
+		this.setState({contacts: parseContacts})
+	}
+}
+
+componentDidUpdate(prevState) {
+	if (this.state.contacts !== prevState.contacts) {
+		localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+	}
+}
+
 addContact = (newContact) => {
 	this.setState((prevState) => ({
 		contacts: [...prevState.contacts, newContact]
