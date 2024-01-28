6e56374759e6c92a2f0032a7fdf4dff4954953fa
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2024-01-28 22:16:11 +0200

    after feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
 import { ContactForm } from "./ContactForm/ContactForm";
 import {ContactList} from "./ContactList/ContactList"
+
+import initialContacts from './contacts.json';
+
 import {Filter} from "./Filter/Filter"
 import { Container, Title } from './App.styled'
 import { GlobalStyle } from "GlobalStyle";
 
+
+const storageKey = 'contacts';
+
 export class App extends Component {
 	state = {
-		contacts: [
-			{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-			{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-			{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-			{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-],
-filter: ''
-}
+		contacts: initialContacts,
+		filter: '',
+};
 
 componentDidMount() {
-	const contacts = localStorage.getItem('contacts');
-	const parseContacts = JSON.parse(contacts);
-	if (parseContacts) {
-		this.setState({contacts: parseContacts})
-	}
+	const savedContacts = window.localStorage.getItem(storageKey);
+		if (savedContacts !== null) {
+		this.setState({
+		contacts: JSON.parse(savedContacts),
+});
+}
 }
 
-componentDidUpdate(prevState) {
-	if (this.state.contacts !== prevState.contacts) {
-		localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-	}
+componentDidUpdate(prevProps, prevState) {
+	if (prevState.contacts !== this.state.contacts) {
+			window.localStorage.setItem(
+			storageKey,
+			JSON.stringify(this.state.contacts)
+);
+}
 }
 
 addContact = (newContact) => {
